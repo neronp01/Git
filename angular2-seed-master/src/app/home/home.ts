bd06a0b8eaddf92799c30905cd0da5a62b6142c6
neronp01 <neronpascal001@gmail.com>
2016-10-01 23:56:09 -0400

    First Load

---
+++ angular2-seed-master/src/app/home/home.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {Movies} from '../movies/movies.component';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.css'],
+  templateUrl: './home.html',
+  directives: [Movies]
+})
+
+export class Home {
+
+  constructor  (){}
+}
---
+++ angular2-seed-master/src/app/movies/movies.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector: 'movies', 
+    templateUrl: './movies.component.html'
+})
+
+export class Movies {
+
+    
+}
